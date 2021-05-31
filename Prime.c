f6b6fa167e13214ebabe2c0aa09f60103fa22692
RAJEET614 <84983566+RAJEET614@users.noreply.github.com>
2021-05-31 22:26:47 +0530

    create prime.c file
    This file prints positive numbers on the screen.

---
+++ Prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int is prime(int,int);
+int main()
+{   
+  int num,prime;
+  printf("Enter a positive number:");
+  scanf("%d",&num);
+  prime=is prime(num.num/2);
+    if(prime==1)
+      printf("%d prime number",num);
+  { 
+    else
+      printf("%d not a prime number",num);
+       return 0;
+  }
+  int is prime(int num,inti){
+    if(i==1)
+    {
+      return 1;
+    }
+    
+    
+        
+ 
